0d6b672e8cd9d90404147220a5cdbce9a1e22d64
Joga50 <jose.gabriel.a12@gmail.com>
2023-02-07 15:10:25 -0500

    setting routes and controller folders, with their respective files

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
 import express from "express";
+import postRoutes from "./routes/posts.js";
+// import userRoutes from "./routes/users.js";
+// import authRoutes from "./routes/auth.js";
 
 const app = express();
 
 app.use(express.json());
+
+// POSTS ROUTE
+app.use("/api/posts", postRoutes);
+// USER ROUTE
+// app.use("/api/users", userRoutes);
+// AUTH ROUTE
+// app.use("/api/auth", authRoutes);
+
+app.get("/test", (req, res) => {
+  res.json("working");
+});
 
 app.listen(8800, () => {
   console.log("connected");
